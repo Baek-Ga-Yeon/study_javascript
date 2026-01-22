dc77ce73d435e492eb53121ddc138daf7df54af2
Baek-Ga-Yeon <90228141+Baek-Ga-Yeon@users.noreply.github.com>
2026-01-22 18:06:45 +0900

    Add files via upload

---
+++ ObjectMapping.js
@@ -0,0 +1,13 @@
+const order = [
+  { id : 1, status : "paid" },
+  { id : 2, status : "shipping" },
+  { id : 3, status : "done" }
+];
+let StatusMap = {
+  paid : "결제 완료",
+  shipping : "배송 중",
+  done : "배송 완료"
+}
+
+let test = order.map(o => `주문 ${o.id} ` + ": " + `${StatusMap[o.status]}`);
+console.log(test);
