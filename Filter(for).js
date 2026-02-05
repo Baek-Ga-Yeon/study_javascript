1ba944d25a43d7ef52637ff40caad76c745ddfe4
Ga-Yeon-Baek <flyy11@naver.com>
2026-02-05 15:23:33 +0900

    Update Filter(for).js

--- Filter(for).js
+++ Filter(for).js
@@ -14,4 +14,21 @@ for (let i = 0; i < users.length; i++) {
 
 console.log(result)
 
-// users.filter(u => u.isActive && !u.isBlocked);
+// 화살표 함수
+
+users.filter(u => u.isActive && !u.isBlocked);
+
+
+// 실무 스타일
+
+const users = [
+  { name: "가연", isActive: true, isBlocked: false },
+  { name: "서환", isActive: true, isBlocked: true },
+  { name: "이지", isActive: false, isBlocked: false },
+];
+
+function test(user) {
+  return user.isActive && user.isBlocked === false
+};
+const result = users.filter(test);
+console.log(result);
