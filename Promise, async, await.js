f9e0fc0ba9b7ff5cb5ffd9289b7e719459fe23ef
Ga-Yeon-Baek <flyy11@naver.com>
2026-02-15 22:23:14 +0900

    Feat : Promise 실무 실습

--- Promise, async, await.js
+++ Promise, async, await.js
@@ -1,33 +1,111 @@
-// 1
+// // 1
 
-function delayVelue() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("완료");
-    }, 1000);
-  });
-}
+// function delayVelue() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("완료");
+//     }, 1000);
+//   });
+// }
 
-async function run() {
-  const result = await delayVelue();
-  console.log(result);
-}
+// async function run() {
+//   const result = await delayVelue();
+//   console.log(result);
+// }
 
-run();
+// run();
 
-// 2
+// // 2
 
-function addLater(a, b) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(a + b);
-    }, 2000);
-  });
-}
+// function addLater(a, b) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(a + b);
+//     }, 2000);
+//   });
+// }
 
-async function run() {
-  const test = await addLater(3, 7);
-  console.log(test);
-}
+// async function run() {
+//   const test = await addLater(3, 7);
+//   console.log(test);
+// }
 
-run();
+// run();
+
+// 3
+
+// function sleep(ms) {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve("값은 여기"),ms);
+//     });
+// }
+
+// async function go(){
+//     const test = await sleep(3000)
+//     console.log(test)
+//     console.log("성공")
+// }
+// go();
+
+// 4
+
+// function fetchUser() {
+//     return new Promise((resolve) => {
+//         setTimeout(()=>{
+//             resolve({ id : 1, name : "가연" })
+//         },1000);
+//     });
+// }
+
+//  async function go2() {
+//     const test = await fetchUser()
+//      console.log(test)
+// }
+//  go2()
+
+// 5
+
+// function fetchPost() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             reject("서버 오류")
+//         }, 1000);
+//     })
+// };
+
+// async function run2() {
+//     try {
+//         const post = await fetchPost()
+//         console.log(post);
+//     }
+//     catch(err) {
+//         console.log("에러 : ", err)
+//     }
+// }
+// run2();
+
+// 6
+
+// function otherUser() {
+//     return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve("유저")
+//             },1000);
+//     })
+// };
+// function otherPost() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("게시글")
+//         },1000)
+//     })
+// }
+
+// async function others() {
+//     const user = await otherUser()
+//     console.log(user);
+
+//     const post = await otherPost()
+//     console.log(post);
+// };
+// others()
